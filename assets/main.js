b443716f0188cf4e101738c61810ea15f4b84fbe
Kiah Reading <kiah.reading@gmail.com>
2024-01-05 14:51:42 +1100

    js added

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,48 @@
-var test = document.getElementById('test')
+import Splide from '@splidejs/splide';
+import LazyLoad from "vanilla-lazyload";
+
+
+// --------------------------------------------------
+// 💤 Lazy Loading
+// --------------------------------------------------
+
+const lazyLoadInstance = new LazyLoad({
+  // Your custom settings go here
+  elements_selector: "[data-lazy]",
+  callback_loaded: (el) => {
+    gsap.to(el, {
+      autoAlpha: 1,
+      duration: .5,
+      ease: 'power1.inOut',
+      onComplete: () => {
+        el.dispatchEvent(new Event('lazyloaded'));
+      }
+    })
+  }
+});
+
+// --------------------------------------------------
+// 🎠 Carousels
+// --------------------------------------------------
+
+let bannerCarousel = document.querySelector('.splide--banner');
+
+if (bannerCarousel) {
+  let bannerSplide = new Splide(bannerCarousel, {
+    type: 'fade',
+    rewind: true,
+    speed: 1200,
+    pauseOnHover: false,
+    interval: 3400,
+    autoplay: true,
+    arrows: false,
+    pagination: false,
+    perPage: 1,
+    perMove: 1,
+    focus: 'center',
+    width: '100%',
+    updateOnMove: true
+  });
+
+  bannerSplide.mount();
+}
